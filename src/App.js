7507994a695b1c42abf32a4f4c84c2602ce25352
aselwilliams <aselwilliams22@gmail.com>
2022-06-04 12:13:14 -0400

    btns work now

--- src/App.js
+++ src/App.js
@@ -3,18 +3,32 @@ import './App.css';
 import data from './data'
 
 function App() {
+  const [teams, setTeams]=useState(data)
+
+  const handleVote=(index)=>{
+    const newTeams=[...teams]
+    newTeams[index].vote+=1;
+    setTeams(newTeams)
+  }
+
+  const handleClear=(index)=>{
+    const newTeams=teams.map((t)=>t)
+    console.log(newTeams)
+    newTeams[index].vote=0;
+    setTeams(newTeams)
+  }
   return (
     <div className="container">
-    {this.state.teams.map((team, index)=> {
+    {teams.map((team, index)=> {
     return (
       <div className="card" key={team.name}>
         <img src={team.img} alt={`${team.name} logo`} />
         <h3>{team.name}</h3>
-        <button onClick={()=>this.handleVote(index)}>Vote</button>
+        <button onClick={()=>handleVote(index)}>Vote</button>
         
     <div className="lower">
         <p>Vote count: <span id="span1">{team.vote}</span></p>
-        <button onClick={()=>this.handleClear(index)} type="reset" className='clear-btn'>Clear</button>
+        <button onClick={()=>handleClear(index)} type="reset" className='clear-btn'>Clear</button>
     </div>
     </div>
     )
